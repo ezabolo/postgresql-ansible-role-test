2feb47351d43df776a259c8f338a68d3def71283
Eugene Zabolo <eugene.zabolo@gmail.com>
2019-07-12 02:17:38 -0400

    adding security group for dev vpc

--- terraform-code1/security_groups.tf
+++ terraform-code1/security_groups.tf
@@ -33,3 +33,42 @@ tags {
     Name = "prod-secgroup"
   }
 }
+
+#Security Group for dev vpc
+
+resource "aws_security_group" "prod-postgres-secgroup" {
+  vpc_id = "${aws_vpc.vpc-dev.id}"
+  name = "dev-postgres-secgroup"
+  description = "security group that allows ping, ssh, posgres and all egress traffic"
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+      from_port = 5432
+      to_port = 5432
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+      from_port = 8
+      to_port = 0
+      protocol = "icmp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+tags {
+    Name = "dev-secgroup"
+  }
+}
+
