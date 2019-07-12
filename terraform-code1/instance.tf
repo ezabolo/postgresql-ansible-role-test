c22de586d128ba579c6dff092ae2629257d711ca
Eugene Zabolo <eugene.zabolo@gmail.com>
2019-07-12 02:07:01 -0400

    adding dev infrastructure

--- terraform-code1/instance.tf
+++ terraform-code1/instance.tf
@@ -31,3 +31,36 @@ resource "aws_instance" "postgres-prod-node1" {
 
 }
 
+#Dev postgresql server
+
+resource "aws_instance" "postgres-dev-node1" {
+  ami = "ami-006219aba10688d0b"
+  instance_type = "t2.medium"
+  key_name = "${aws_key_pair.mykey.key_name}"
+
+#Specifying the subnet id for prod server
+  subnet_id = "${aws_subnet.dev-public-1.id}"
+
+  vpc_security_group_ids = ["${aws_security_group.dev-postgres-secgroup.id}"]
+
+  provisioner "file" {
+    source = "update-server.sh"
+    destination = "/tmp/update-server.sh"
+  }
+
+  provisioner "remote-exec" {
+   inline = [
+     "chmod +x /tmp/update-server.sh",
+     "sudo /tmp/update-server.sh"
+   ]
+  }
+  connection {
+    user = "${var.INSTANCE_USERNAME}"
+    private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
+  }
+
+ tags { Name = "postgres-dev-node1"
+    }
+
+}
+
--- terraform-code1/vpc.tf
+++ terraform-code1/vpc.tf
@@ -109,3 +109,118 @@ resource "aws_route_table_association" "prod-public-3-a" {
     route_table_id = "${aws_route_table.prod-public.id}"
 }
 
+
+#VPC for Dev environment
+# Dev vpc
+resource "aws_vpc" "vpc-dev" {
+    cidr_block = "172.17.0.0/16"
+    instance_tenancy = "default"
+    enable_dns_support = "true"
+    enable_dns_hostnames = "true"
+    enable_classiclink = "false"
+    tags {
+        Name = "vpcdev"
+    }
+}
+
+
+# Subnets
+resource "aws_subnet" "dev-public-1" {
+    vpc_id = "${aws_vpc.vpc-dev.id}"
+    cidr_block = "172.17.1.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1a"
+
+    tags {
+        Name = "dev-public-1"
+    }
+}
+resource "aws_subnet" "dev-public-2" {
+    vpc_id = "${aws_vpc.vpc-dev.id}"
+    cidr_block = "172.17.2.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1b"
+
+    tags {
+        Name = "dev-public-2"
+    }
+}
+resource "aws_subnet" "dev-public-3" {
+    vpc_id = "${aws_vpc.vpc-dev.id}"
+    cidr_block = "172.17.3.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1c"
+
+    tags {
+        Name = "dev-public-3"
+    }
+}
+resource "aws_subnet" "dev-private-1" {
+    vpc_id = "${aws_vpc.vpc-dev.id}"
+    cidr_block = "172.17.4.0/24"
+    map_public_ip_on_launch = "false"
+    availability_zone = "us-east-1a"
+
+    tags {
+        Name = "dev-private-1"
+    }
+}
+resource "aws_subnet" "dev-private-2" {
+    vpc_id = "${aws_vpc.vpc-dev.id}"
+    cidr_block = "172.17.5.0/24"
+    map_public_ip_on_launch = "false"
+    availability_zone = "us-east-1b"
+
+    tags {
+        Name = "dev-private-2"
+    }
+}
+resource "aws_subnet" "dev-private-3" {
+    vpc_id = "${aws_vpc.vpc-dev.id}"
+    cidr_block = "172.17.6.0/24"
+    map_public_ip_on_launch = "false"
+    availability_zone = "us-east-1c"
+
+    tags {
+        Name = "dev-private-3"
+    }
+}
+
+# Internet GW
+resource "aws_internet_gateway" "dev-gw" {
+    vpc_id = "${aws_vpc.vpc-dev.id}"
+
+    tags {
+        Name = "dev-gw"
+    }
+}
+
+# route tables
+resource "aws_route_table" "dev-public" {
+    vpc_id = "${aws_vpc.vpc-dev.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.dev-gw.id}"
+    }
+
+    tags {
+        Name = "dev-public-route"
+    }
+}
+
+# route associations public
+resource "aws_route_table_association" "dev-public-1-a" {
+    subnet_id = "${aws_subnet.dev-public-1.id}"
+    route_table_id = "${aws_route_table.dev-public.id}"
+}
+resource "aws_route_table_association" "dev-public-2-a" {
+    subnet_id = "${aws_subnet.dev-public-2.id}"
+    route_table_id = "${aws_route_table.dev-public.id}"
+}
+resource "aws_route_table_association" "dev-public-3-a" {
+    subnet_id = "${aws_subnet.dev-public-3.id}"
+    route_table_id = "${aws_route_table.dev-public.id}"
+}
+
+
+
